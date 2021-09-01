1890d7605b4e7582879810bbeac33bae0cf8f8c9
Samuel Rodrigues <samuelclerod@gmail.com>
2021-08-31 21:11:03 -0300

    add some error handler

---
+++ config/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // http://api.weatherstack.com/current?access_key=adc237cd619def2e5e8cf3ed7c8ef77e&query=-7.2237358,-39.3265404
+  access_key: 'adc237cd619def2e5e8cf3ed7c8ef77e',
+  base_url: 'http://api.weatherstack.com/current',
+}
--- utils/weatherService.js
+++ utils/weatherService.js
@@ -1,16 +1,19 @@
 const request = require('postman-request')
-const base_url = 'http://api.weatherstack.com/current'
+const { access_key, base_url } = require('../config/constants')
+
 const lat = '-7.2237358', lon = "-39.3265404"
 
 const url = `${base_url}?access_key=${access_key}&query=${lat},${lon}`
 // imprimir "It is currently  y degrees out. There is a X% chance of rain.
-request(url, (error, response) => {
+request({ url, json: true }, (error, response) => {
   if (error) {
-    return console.error(error)
+    return console.log("Unavailable weather service")
+  }
+  if (response.body.error) {
+    return console.log("Invalid coordinates")
   }
 
-  const data = JSON.parse(response.body)
-  const { temperature, feelslike, weather_descriptions, precip } = data.current
+  const { temperature, feelslike, weather_descriptions, precip } = response.body.current
   const weather = weather_descriptions[0]
 
   let outputText = `It is currently  ${temperature} degrees out, and feels like`
