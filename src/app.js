24c43d7bdf20131b8811889a2ee6351581c65dcd
Samuel Rodrigues <samuelclerod@gmail.com>
2021-09-21 21:41:02 -0300

    add template engine

--- src/app.js
+++ src/app.js
@@ -1,17 +1,44 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 const dotenv = require('dotenv')
-
 dotenv.config()
 
-const constants = require('../config/constants')
+const { port } = require('../config/constants').server
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 const app = express()
+
+// template engine configuration
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//static assets configuration
 app.use(express.static(publicDirectoryPath))
 
-//TODO add template engine: ejs, pug, hbs(handlebars)
+// endpoints
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: '🏡 Home',
+  })
+})
 
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: '🙇‍♂️ Help!',
+    helpMessage: 'Aqui você encontra um pouco de ajuda...',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: '👨‍🦲 About me ',
+    name: 'Samuel Rodrigues',
+  })
+})
 app.get('/weather', (req, res) => {
   res.json({
     forecast: 'minha previsão do tempo',
@@ -22,13 +49,20 @@ app.get('/weather', (req, res) => {
   })
 })
 
-const port = constants.server.port
+app.get('/help/*', (req, res) => {
+  res.send("Tópico de ajuda não encontrado")
+})
+
+app.get('*', (req, res) => {
+  res.send("404 Page not found")
+})
 
 app.listen(port, () => {
   console.log(`🚀 Starting app on port ${port}...`)
 })
 
-
+// TODO: Criar pagina personalizada 404 para geral, que permita receber mensagens diferentes 
+// para ser usada tanto na rota "*" quanto na rota "/help/*"
 
 
 
